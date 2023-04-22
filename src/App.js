c57ab7b679687ab7d71df8a44d1a328d92479735
Barthélemy Jouve <jouveb@gmail.com>
2023-04-22 18:40:34 +0200

    nodeReactPremierProjet

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// const App = () => {
+//   return (
+//     <h1 className="title">Men's Essentials</h1>
+//   )
+// };
+
+// export default App;
+
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Men's Essentials</h1>
+    </div>
+  );
+};
+
+export default App;
