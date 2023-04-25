063ffc52eff9b8a9b5920e3fdd22d99178139d2c
Barthélemy Jouve <jouveb@gmail.com>
2023-04-25 18:13:01 +0200

    blog

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,9 +1,48 @@
 import React from "react";
 
-const Article = () => {
+const articleContent = {
+  articles: [
+    {
+      id: 1,
+      title: "Titre de l'article 1",
+      content: "Contenu de l'article 1",
+      image: "https://example.com/image1.jpg",
+    },
+    {
+      id: 2,
+      title: "Titre de l'article 2",
+      content: "Contenu de l'article 2",
+      image: "https://example.com/image2.jpg",
+    },
+    {
+      id: 3,
+      title: "Titre de l'article 3",
+      content: "Contenu de l'article 3",
+      image: "https://example.com/image2.jpg",
+    },
+    {
+      id: 4,
+      title: "Titre de l'article 4",
+      content: "Contenu de l'article 4",
+      image: "https://example.com/image2.jpg",
+    },
+    {
+      id: 5,
+      title: "Titre de l'article 5",
+      content: "Contenu de l'article 5",
+      image: "https://example.com/image2.jpg",
+    },
+  ],
+};
+
+const Article = ({ id }) => {
+  const article = articleContent.articles.find((article) => article.id === id);
+
   return (
-    <div className="article-blog">
-      <h3>Article de test</h3>
+    <div>
+      <h2>{article.title}</h2>
+      <img src={article.image} alt={article.title} />
+      <p>{article.content}</p>
     </div>
   );
 };
--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -8,43 +8,24 @@ const Blog = () => {
     <div className="blog-page">
       <header>
         <Navigation />
-        <h1>Blog</h1>
       </header>
       <main>
-        <Article />
+        <div className="article-container">
+          <Article id={1} />
+        </div>
+        <div className="article-container">
+          <Article id={2} />
+        </div>
+        <div className="article-container">
+          <Article id={3} />
+        </div>
+        <div className="article-container">
+          <Article id={4} />
+        </div>
+        <div className="article-container">
+          <Article id={5} />
+        </div>
       </main>
-      <div>
-        <h2>About men's skincare</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
-          suscipit sapiente saepe mollitia quis sequi quo ipsa dolores
-          voluptatum sit!
-        </p>
-      </div>
-      <div>
-        <h2>The best tools for dental hygiene</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
-          suscipit sapiente saepe mollitia quis sequi quo ipsa dolores
-          voluptatum sit!
-        </p>
-      </div>
-      <div>
-        <h2>The best electronics items</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
-          suscipit sapiente saepe mollitia quis sequi quo ipsa dolores
-          voluptatum sit!
-        </p>
-      </div>
-      <div>
-        <h2>Accessories to have</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
-          suscipit sapiente saepe mollitia quis sequi quo ipsa dolores
-          voluptatum sit!
-        </p>
-      </div>
       <Footer />
     </div>
   );
